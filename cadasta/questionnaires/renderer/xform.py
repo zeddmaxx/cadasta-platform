9e35baa60996a3496e8813f1a5d4fa4de817fffa
Oliver Roick <oliver.roick@gmail.com>
2016-09-26 17:29:19 +0200

    XML Rendering -- new approach

--- cadasta/questionnaires/renderer/xform.py
+++ cadasta/questionnaires/renderer/xform.py
@@ -1,102 +1,72 @@
-import xml.etree.ElementTree as ET
+from pyxform.builder import create_survey_element_from_dict
+from lxml import etree
 from rest_framework.renderers import BaseRenderer
+from ..models import Question
 
-MEDIA_TYPES = {
-    'PH': 'image/*',
-    'AU': 'audio/*',
-    'VI': 'video/*'
-}
+QUESTION_TYPES = dict(Question.TYPE_CHOICES)
 
 
 class XFormRenderer(BaseRenderer):
     format = 'xform'
 
-    def create_option(self, option):
-        item = ET.Element('item')
-        label = ET.SubElement(item, 'label')
-        label.text = option.get('label')
-        value = ET.SubElement(item, 'value')
-        value.text = option.get('name')
+    def transform_questions(self, questions):
+        children = []
+        for q in questions:
+            q['type'] = QUESTION_TYPES[q['type']]
+            if 'options' in q:
+                q['choices'] = q['options']
+            children.append(q)
+        return children
 
-        return item
+    def transform_groups(self, groups):
+        transformed_groups = []
+        for g in groups:
+            group = {
+                'type': 'group',
+                'name': g.get('name'),
+                'label': g.get('label'),
+                'children': self.transform_questions(g.get('questions'))
+            }
+            transformed_groups.append(group)
+        return transformed_groups
 
-    def create_input(self, id, question):
-        attrs = {'ref':  '/' + id + '/' + question.get('name')}
-        tag = 'input'
+    def transform_to_xform_json(self, data):
+        json = {
+            'default_language': 'default',
+            'name': data.get('id_string'),
+            'sms_keyword': data.get('id_string'),
+            'type': 'survey',
+            'id_string': data.get('id_string'),
+            'title': data.get('id_string')
+        }
 
-        if question.get('type') in ['PH', 'AU', 'VI']:
-            tag = 'upload'
-            attrs['mediatype'] = MEDIA_TYPES[question.get('type')]
-        elif question.get('type') == 'S1':
-            tag = 'select1'
+        questions = self.transform_questions(data.get('questions', []))
+        question_groups = self.transform_groups(
+            data.get('question_groups', []))
+        json['children'] = questions + question_groups
+        return json
 
-        field = ET.Element(tag, attrs)
-        label = ET.SubElement(field, 'label')
-        label.text = question.get('label')
-
-        if question.get('type') == 'S1':
-            for option in question.get('options'):
-                field.append(self.create_option(option))
-        return field
-
-    def add_questions_to_body(self, id, element, questions):
-        for field in questions:
-            element.append(self.create_input(id, field))
-
-    def add_questiongroups_to_body(self, id, element, question_groups):
-        for group in question_groups:
-            group_id = id + '/' + group.get('name')
-            group_el = ET.SubElement(element,
-                                     'group',
-                                     {'ref':  '/' + group_id})
-            label = ET.SubElement(group_el, 'label')
-            label.text = group.get('label')
-            self.add_questions_to_body(group_id,
-                                       group_el,
-                                       group.get('questions'))
-
-    def add_questions_to_header(self, element, questions):
-        for field in questions:
-            ET.SubElement(element, field.get('name'))
-
-    def add_questiongroups_to_header(self, element, question_groups):
-        for group in question_groups:
-            group_el = ET.SubElement(element, group.get('name'))
-            self.add_questions_to_header(group_el, group.get('questions'))
+    def insert_version_attribute(self, xform, root_node, version):
+        ns = {'xf': 'http://www.w3.org/2002/xforms'}
+        root = etree.fromstring(xform)
+        inst = root.find(
+            './/xf:instance/xf:{root_node}'.format(
+                root_node=root_node
+            ), namespaces=ns
+        )
+        inst.set('version', str(version))
+        xml = etree.tostring(
+            root, method='xml', encoding='utf-8', pretty_print=True
+        )
+        return xml
 
     def render(self, data, **kwargs):
-        root = ET.Element(
-            'h:html',
-            {'xmlns': 'http://www.w3.org/2002/xforms',
-             'xmlns:ev': 'http://www.w3.org/2001/xml-events',
-             'xmlns:h': 'http://www.w3.org/1999/xhtml',
-             'xmlns:jr': 'http://openrosa.org/javarosa',
-             'xmlns:orx': 'http://openrosa.org/xforms',
-             'xmlns:xsd': 'http://www.w3.org/2001/XMLSchema'}
-        )
+        json = self.transform_to_xform_json(data)
+        survey = create_survey_element_from_dict(json)
+        xml = survey.xml().toxml()
 
-        title = ET.Element('h:title')
-        title.text = data.get('id_string')
+        xml = self.insert_version_attribute(xml,
+                                            data.get('filename'),
+                                            data.get('version'))
 
-        model = ET.Element('model')
-        instance = ET.SubElement(model, 'instance')
-        thing = ET.SubElement(instance,
-                              data.get('id_string'),
-                              attrib={'id': data.get('id_string'),
-                                      'version': data.get('version')})
-
-        self.add_questions_to_header(thing, data.get('questions'))
-        self.add_questiongroups_to_header(thing,
-                                          data.get('question_groups', []))
-        head = ET.Element('h:head')
-        head.append(title)
-        head.append(model)
-        root.append(head)
-
-        body = ET.Element('h:body')
-        self.add_questions_to_body(data.get('id_string'),
-                                   body,
-                                   data.get('questions'))
-        root.append(body)
-
-        return ET.tostring(root)
+        return xml
--- cadasta/questionnaires/tests/test_renderer.py
+++ cadasta/questionnaires/tests/test_renderer.py
@@ -1,169 +1,134 @@
-import xml.etree.ElementTree as ET
 from django.test import TestCase
 from ..renderer.xform import XFormRenderer
 
 
 class XFormRendererTest(TestCase):
-    def test_create_input(self):
-        field = {
-            "id": "v4ip2h6hzd96x3schjhh7tfh",
-            "name": "party_name",
-            "label": "Party Name",
-            "type": "TX"
-        }
-        renderer = XFormRenderer()
-        element = renderer.create_input('abc123', field)
-        xml = ET.tostring(element).decode()
-        assert ('<input ref="/abc123/party_name">'
-                '<label>Party Name</label></input>') == xml
-
-    def test_create_upload_input(self):
-        field = {
-            "id": "v4ip2h6hzd96x3schjhh7tfh",
-            "name": "photo",
-            "label": "A photo",
-            "type": "PH"
-        }
-        renderer = XFormRenderer()
-        element = renderer.create_input('abc123', field)
-        xml = ET.tostring(element).decode()
-        assert ('<upload mediatype="image/*" ref="/abc123/photo">'
-                '<label>A photo</label></upload>') == xml
-
-    def test_create_select1_input(self):
-        field = {
-            'id': "byv297rfgrtjcipj6eqirk5e",
-            'name': "location_type",
-            'label': "What is the land feature?",
+    def test_transform_questions(self):
+        questions = [{
+            'id': "xp8vjr6dsk46p47u22fft7bg",
+            'name': "tenure_type",
+            'label': "What is the social tenure type?",
             'type': "S1",
-            'options': [
-                {
-                  'id': "uqu3heeejav74mrzgdw4w6wt",
-                  'name': "MI",
-                  'label': "Miscellaneous"
-                },
-                {
-                  'id': "6azt8zkw45htpf6f9g8csm2j",
-                  'name': "NP",
-                  'label': "National Park Boundary"
-                }
-            ]
-        }
+            'required': False,
+            'constraint': None,
+            'options': [{
+                'id': "d9pkepyjg4sgaepdytgwkgfv",
+                'name': "WR",
+                'label': "Water Rights"
+            }, {
+                'id': "vz9533y64f2rns6v8frssc5v",
+                'name': "UC",
+                'label': "Undivided Co-ownership"
+            }]
+        }, {
+            'id': "bzs2984c3gxgwcjhvambdt3w",
+            'name': "start",
+            'label': None,
+            'type': "ST",
+            'required': False,
+            'constraint': None
+        }]
         renderer = XFormRenderer()
-        element = renderer.create_input('abc123', field)
-        xml = ET.tostring(element).decode()
-        assert ('<select1 ref="/abc123/location_type"><label>What is the land '
-                'feature?</label><item><label>Miscellaneous</label><value>MI'
-                '</value></item><item><label>National Park Boundary</label>'
-                '<value>NP</value></item></select1>') == xml
+        transformed = renderer.transform_questions(questions)
+        assert len(transformed) == 2
+        for q in transformed:
+            if q['name'] == 'start':
+                assert q['type'] == 'start'
+            elif q['name'] == 'tenure_type':
+                assert q['type'] == 'select one'
+                assert q['choices'] == questions[0]['options']
 
-    def test_add_questions_to_body(self):
-        questions = [
-            {
-                "id": "xp8vjr6dsk46p47u22fft7bg",
-                "name": "tenure_type",
-                "label": "Tenure Type",
-                "type": "TX"
-            },
-            {
-                "id": "xp8vjr6dsk46p47u22fft7bg",
-                "name": "party_type",
-                "label": "Party Type",
-                "type": "TX"
-            }
-        ]
-
-        element = ET.Element('root')
-        renderer = XFormRenderer()
-        renderer.add_questions_to_body('abc123', element, questions)
-
-        xml = ET.tostring(element).decode()
-
-        assert '<label>Tenure Type</label>' in xml
-        assert '<label>Party Type</label>' in xml
-
-    def test_add_questiongroups_to_body(self):
-        question_groups = [
-            {
-                'id': "grswcid3t2tyrzq4gsemrhf2",
-                'name': "location_attributes",
-                'label': "Location Attributes",
-                'questions': [{
-                    "id": "xp8vjr6dsk46p47u22fft7bg",
-                    "name": "tenure_type",
-                    "label": "Tenure Type",
-                    "type": "TX"
-                }, {
-                    "id": "xp8vjr6dsk46p47u22fft7bg",
-                    "name": "party_type",
-                    "label": "Party Type",
-                    "type": "TX"
-                }]
-            }
-        ]
-
-        element = ET.Element('root')
-        renderer = XFormRenderer()
-        renderer.add_questiongroups_to_body('abc123', element, question_groups)
-
-        xml = ET.tostring(element).decode()
-
-        assert '<group ref="/abc123/location_attributes">' in xml
-        assert '<label>Tenure Type</label>' in xml
-        assert 'ref="/abc123/location_attributes/tenure_type"' in xml
-        assert '<label>Party Type</label>' in xml
-        assert 'ref="/abc123/location_attributes/party_type"' in xml
-
-    def test_add_questions_to_header(self):
-        questions = [
-            {
-                'id': "xp8vjr6dsk46p47u22fft7bg",
-                'name': "tenure_type",
-            },
-            {
+    def test_transform_groups(self):
+        groups = [{
+            'id': '123',
+            'name': 'group_1',
+            'label': 'Group 2',
+            'questions': [{
                 'id': "bzs2984c3gxgwcjhvambdt3w",
                 'name': "start",
-            }
-        ]
-
-        element = ET.Element('root')
+                'label': None,
+                'type': "ST",
+            }]
+        }, {
+            'id': '456',
+            'name': 'group_2',
+            'label': 'Group 2',
+            'questions': [{
+                'id': "xp8vjr6dsk46p47u22fft7bg",
+                'name': "tenure_type",
+                'label': "What is the social tenure type?",
+                'type': "TX",
+            }]
+        }]
         renderer = XFormRenderer()
-        renderer.add_questions_to_header(element, questions)
+        transformed = renderer.transform_groups(groups)
+        assert len(transformed) == 2
+        for g in transformed:
+            assert g['type'] == 'group'
+            assert len(g['children']) == 1
 
-        xml = ET.tostring(element).decode()
-
-        assert '<tenure_type />' in xml
-        assert '<start />' in xml
-
-    def test_add_questiongroups_to_header(self):
-        question_groups = [
-            {
-                'id': "grswcid3t2tyrzq4gsemrhf2",
-                'name': "location_attributes",
-                'label': "Location Attributes",
+    def test_transform_to_xform_json(self):
+        data = {
+            'id_string': 'abc123',
+            'version': '1234567890',
+            'filename': 'abc123',
+            'questions': [
+                {
+                  'id': "xp8vjr6dsk46p47u22fft7bg",
+                  'name': "tenure_type",
+                  'label': "What is the social tenure type?",
+                  'type': "S1",
+                  'required': False,
+                  'constraint': None,
+                  'options': [
+                    {
+                      'id': "d9pkepyjg4sgaepdytgwkgfv",
+                      'name': "WR",
+                      'label': "Water Rights"
+                    },
+                    {
+                      'id': "vz9533y64f2rns6v8frssc5v",
+                      'name': "UC",
+                      'label': "Undivided Co-ownership"
+                    },
+                  ]
+                }
+            ],
+            'question_groups': [{
+                'id': '123',
+                'name': 'group_1',
+                'label': 'Group 2',
                 'questions': [{
-                    'id': "xp8vjr6dsk46p47u22fft7bg",
-                    'name': "tenure_type",
-                }, {
                     'id': "bzs2984c3gxgwcjhvambdt3w",
                     'name': "start",
+                    'label': None,
+                    'type': "ST",
                 }]
-            }
-        ]
-
-        element = ET.Element('root')
+            }, {
+                'id': '456',
+                'name': 'group_2',
+                'label': 'Group 2',
+                'questions': [{
+                    'id': "xp8vjr6dsk46p47u22fft7bg",
+                    'name': "party_type",
+                    'label': "What is the party type?",
+                    'type': "TX",
+                }]
+            }]
+        }
         renderer = XFormRenderer()
-        renderer.add_questiongroups_to_header(element, question_groups)
-
-        xml = ET.tostring(element).decode()
-
-        assert ('<location_attributes><tenure_type /><start />'
-                '</location_attributes>') in xml
+        transformed = renderer.transform_to_xform_json(data)
+        assert transformed['name'] == 'abc123'
+        assert transformed['sms_keyword'] == 'abc123'
+        assert transformed['id_string'] == 'abc123'
+        assert transformed['title'] == 'abc123'
+        assert len(transformed['children']) == 3
 
     def test_render(self):
         data = {
             'id_string': 'abc123',
             'version': '1234567890',
+            'filename': 'abc123',
             'questions': [
                 {
                   'id': "xp8vjr6dsk46p47u22fft7bg",
@@ -197,6 +162,5 @@ class XFormRendererTest(TestCase):
         }
         renderer = XFormRenderer()
         xml = renderer.render(data).decode()
-        print(xml)
         assert '<h:title>abc123</h:title>' in xml
         assert '<abc123 id="abc123" version="1234567890">' in xml
